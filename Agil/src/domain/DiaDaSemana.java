5c479cf62e85183c8c1da6644eb3af3213e0002c
martim <martim00@gmail.com>
2012-11-09 16:30:14 -0200

    deletando arquivos da pasta Agil, pois agora estao no raiz

--- Agil/src/domain/DiaDaSemana.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public enum DiaDaSemana {
-	SEGUNDA_FEIRA, TERCA_FEIRA, QUARTA_FEIRA, QUINTA_FEIRA, SEXTA_FEIRA
-}
--- Agil/src/domain/HorarioNoDia.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public enum HorarioNoDia {
-	PRIMEIRO_HORARIO, SEGUNDO_HORARIO, TERCEIRO_HORARIO, QUARTO_HORARIO
-}
